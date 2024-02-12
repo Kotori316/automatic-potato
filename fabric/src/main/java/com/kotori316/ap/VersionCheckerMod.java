8dd4045a08a20a8556b9748b95419a20424d187d
Kotori316 <Kotori316@gmail.com>
2024-02-12 23:44:13 +0900

    Check with custom value

--- fabric/src/main/java/com/kotori316/ap/VersionCheckerMod.java
+++ fabric/src/main/java/com/kotori316/ap/VersionCheckerMod.java
@@ -9,8 +9,10 @@ import net.fabricmc.loader.api.metadata.ModMetadata;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.net.URI;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
+import java.util.stream.Stream;
 
 public final class VersionCheckerMod implements ModInitializer {
     public static final String MOD_ID = "kotori316_version_checker";
@@ -19,18 +21,39 @@ public final class VersionCheckerMod implements ModInitializer {
 
     @Override
     public void onInitialize() {
-        List<EntrypointContainer<VersionCheckerEntrypoint>> list = FabricLoader.getInstance().getEntrypointContainers(KEY, VersionCheckerEntrypoint.class);
         String loaderVersion = FabricLoader.getInstance().getModContainer("fabricloader").map(ModContainer::getMetadata).map(ModMetadata::getVersion).map(Version::getFriendlyString).orElse("none");
         String minecraftVersion = FabricLoader.getInstance().getModContainer("minecraft").map(ModContainer::getMetadata).map(ModMetadata::getVersion).map(Version::getFriendlyString).orElse("none");
-
-        CompletableFuture<?>[] versions = list.stream()
+        List<EntrypointContainer<VersionCheckerEntrypoint>> list = FabricLoader.getInstance().getEntrypointContainers(KEY, VersionCheckerEntrypoint.class);
+        Stream<ModWithVersion> fromEntryPoint = list.stream()
             .map(e -> new ModWithVersion(
                 e.getProvider().getMetadata().getId(),
                 e.getProvider().getMetadata().getVersion(),
                 e.getEntrypoint().versionJsonUrl(),
                 e.getEntrypoint().targetMinecraftVersion().orElse(minecraftVersion),
                 loaderVersion
-            ))
+            ));
+        Stream<ModWithVersion> fromCustom = FabricLoader.getInstance()
+            .getAllMods()
+            .stream()
+            .map(ModContainer::getMetadata)
+            .filter(m -> m.containsCustomValue(KEY))
+            .flatMap(m -> {
+                try {
+                    String uri = m.getCustomValue(KEY).getAsString();
+                    return Stream.of(new ModWithVersion(
+                        m.getId(),
+                        m.getVersion(),
+                        URI.create(uri),
+                        minecraftVersion,
+                        loaderVersion
+                    ));
+                } catch (RuntimeException e) {
+                    LOGGER.warn("Invalid configuration of {} in {}", KEY, m.getId());
+                    return Stream.empty();
+                }
+            });
+
+        CompletableFuture<?>[] versions = Stream.concat(fromEntryPoint, fromCustom)
             .map(ModWithVersion::asRunnable)
             .map(CompletableFuture::runAsync)
             .toArray(CompletableFuture[]::new);
