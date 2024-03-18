a28bf472357cb58d8e50378e74a183cb76ecbf09
Kotori316 <Kotori316@gmail.com>
2024-03-18 22:14:02 +0900

    Add docs

--- fabric/src/main/java/com/kotori316/ap/api/VersionCheckerEntrypoint.java
+++ fabric/src/main/java/com/kotori316/ap/api/VersionCheckerEntrypoint.java
@@ -6,20 +6,41 @@ import java.net.URI;
 import java.util.Optional;
 
 public interface VersionCheckerEntrypoint {
+
+    /**
+     * @return the valid URL of version json
+     */
     URI versionJsonUrl();
 
+    /**
+     * You can override this method to change the minecraft version to get mod version.
+     * This value is used as JSON key, like {@code (target version)-latest} or {@code (target version)-recommended} to get the latest version.
+     *
+     * @return {@link Optional#empty()} to use current minecraft version. {@link Optional#of(Object)} to "fix" the version.
+     */
     default Optional<String> targetMinecraftVersion() {
         return Optional.empty();
     }
 
+    /**
+     * @return whether to enable version check of this mod
+     */
     default boolean enabled() {
         return true;
     }
 
+    /**
+     * This method is called after version is compared. The default action is just logging out, but you can change by overriding this method.
+     *
+     * @param holder the result of comparison.
+     */
     default void log(VersionStatusHolder holder) {
         logVersionInfo(holder);
     }
 
+    /**
+     * The default action, independent of actual minecraft version.
+     */
     static void logVersionInfo(VersionStatusHolder holder) {
         switch (holder.versionStatus()) {
             case LATEST:
